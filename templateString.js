5f9ef78853421c958b8abe4c741c0547a9883989
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2022-07-07 12:31:02 -0300

    Create templateString.js

---
+++ templateString.js
@@ -0,0 +1,13 @@
+const nome = 'Pamella'
+
+const concatenacao = 'Olá ' + nome
+console.log(concatenacao)
+
+const template = `Ola 
+${nome} `
+console.log(template)
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
