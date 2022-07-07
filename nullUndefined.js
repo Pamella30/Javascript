5b6f12ea9da68a1bfb3f24365fcf286acacef723
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2022-07-07 18:22:23 -0300

    Create nullUndefined.js

---
+++ nullUndefined.js
@@ -0,0 +1,16 @@
+let valor // nao inicializada
+console.log(valor)
+
+valor = null //ausencia de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = undefined // evitar atribuir undefined
+console.log(!!produto.preco)
+console.log(produto)
+
+produto.preco = null //sem preco 
+console.log(produto)
