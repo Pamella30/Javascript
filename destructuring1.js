a1f2b3cc2fb3baeb35c576a698aaa51c46545fe8
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2022-07-15 16:58:45 -0300

    Create destructuring1.js

---
+++ destructuring1.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Pamella',
+    idade: 32,
+    endereco: {
+        logradouro: 'Bolton Street',
+        numero: 61
+    }
+}
+const {nome, idade} = pessoa
+console.log(nome, idade)
