ac6a26c8bdc899da3c64bb4d7ab1c3d03e9dcdb8
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2022-07-19 20:35:58 -0300

    Create operadores.js

---
+++ operadores.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [2, 7, 14, 9]
+
+const soma = a + b + c + d
+const subtracao = a - b -c - d
+const multiplicacao = a * b 
+const divisao = c / b
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
